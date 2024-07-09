2d576a6e1a9ecb3e58b0c949df6613dde680e3c8
xuantan22 <144461824+xuantan22@users.noreply.github.com>
2024-07-09 15:07:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ app.use(cors());
 
 
 mongoose
-    .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    socketTimeoutMS: 30000, // 30 seconds
-    connectTimeoutMS: 30000 // 30 seconds
-})
+    .connect(process.env.MONGODB_URL)
     .then(() => console.log('Connect to mongoDB successful!'))
     .catch((err) => console.log(err));
 
